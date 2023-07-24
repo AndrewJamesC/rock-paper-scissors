2e8597e74b11b763e3603edb0c3ad762a9feda5b
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2023-07-24 15:42:15 +1200

    Write a function that plays a single round
    The function plays a single round of human vs computer rock paper
    scissors and outputs a message showing win,loss, or draw.

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
 console.log(getComputerChoice());
+
+// A function that plays a single round of rock, paper, scissors with
+// a human player against the computer. Outputs a winner at the end.
+
+// Make sure to make playerSelection case insensitive later
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
+        return `You win ${playerSelection} beats ${computerSelection}!`
+    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "scissors")) {
+        return `You lose ${computerSelection} beats ${playerSelection}!`
+    } else {
+        return "It's a draw! Try again."
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
