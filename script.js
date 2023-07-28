66b849c4ffd7d6b063abb88001fd5aec560a1cf5
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2023-07-28 15:12:51 +1200

    playRound() add a point to the winner of the round git status q

--- script.js
+++ script.js
@@ -10,13 +10,16 @@ function getComputerChoice() {
 // A function that plays a single round of rock, paper, scissors with
 // a human player against the computer. Outputs a winner at the end.
 
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") || (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock")) {
-       return "playerWins"
+      playerScore = playerScore + 1
+      return "playerWins"
     } else if ((playerSelection.toLowerCase() == "rock" && computerSelection == "paper") || (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") || (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors")) {
-       return "computerWins"
+      computerScore = computerScore + 1
+      return "computerWins"
     } else {
        return "draw"
     }
@@ -27,5 +30,13 @@ console.log(playRound(playerSelection, computerSelection));
 
 // A function that plays the above function five times. Keeping score 
 // after each round. After five rounds it displays the winner.
+
+
+
+function game() 
+ 
+   
+   console.log(playerScore);
+   console.log(computerScore);
 
 
